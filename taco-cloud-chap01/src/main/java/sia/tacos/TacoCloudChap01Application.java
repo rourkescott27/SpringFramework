9647f531b97438f4f91d635387c32b7c10df1c22
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-08 13:24:42 +0200

    chapter01

---
+++ taco-cloud-chap01/src/main/java/sia/tacos/TacoCloudChap01Application.java
@@ -0,0 +1,13 @@
+package sia.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudChap01Application {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(TacoCloudChap01Application.class, args);
+    }
+
+}
---
+++ taco-cloud-chap01/src/test/java/sia/tacos/TacoCloudChap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudChap01ApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
