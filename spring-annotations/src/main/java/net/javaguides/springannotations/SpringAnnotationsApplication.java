04bffbe431d75a85fa89ba837b96c1a6b80cbf78
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-09 21:30:22 +0200

    Stereotype Annotations

--- spring-annotations/src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
@@ -1,6 +1,9 @@
 package net.javaguides.springannotations;
 
+import net.javaguides.springannotations.controller.MyController;
 import net.javaguides.springannotations.controller.PizzaController;
+import net.javaguides.springannotations.repository.MyRepository;
+import net.javaguides.springannotations.service.MyService;
 import net.javaguides.springannotations.service.VegPizza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +13,17 @@ public class SpringAnnotationsApplication {
 
     public static void main ( String[] args ) {
         var context = SpringApplication.run(SpringAnnotationsApplication.class, args);
-        PizzaController pizzaController = (PizzaController) context.getBean("pizzaController");
-        System.out.println(pizzaController.getPizza());
+//        PizzaController pizzaController = (PizzaController) context.getBean("pizzaController");
+//        System.out.println(pizzaController.getPizza());
 
+        MyController myController = context.getBean(MyController.class);
+        System.out.println(myController.hello());
+
+        MyService myService = context.getBean(MyService.class);
+        System.out.println(myService.hello());
+
+        MyRepository myRepository = context.getBean(MyRepository.class);
+        System.out.println(myRepository.hello());
 //        VegPizza vegPizza = (VegPizza) context.getBean("vegPizza");
 //        System.out.println(vegPizza.getPizza());
     }
