ee98f4409b7de48746a420a8a7892a82ef9c92d4
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-10 14:25:34 +0200

    @RestController Annotation

--- spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -4,18 +4,21 @@ import net.javaguides.springannotations.beans.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
 public class BookController {
 
     @RequestMapping("/hello-world")
-    @ResponseBody
+    //@ResponseBody
     public String helloWorld(){
         return "Hello World";
     }
 
     @RequestMapping("/book")
-    @ResponseBody
+    //@ResponseBody
     public Book getBook(){
         Book book = new Book(1, "The Compound Effect", "The Compound Effect by Darren Hardy is a self-help book that shows how small, consistent actions—good or bad—compound over time to produce significant long-term results in your life, finances, health, and habits");
         return book;
