be71d01cb7663033ae8ec102306b23e603cacdcf
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-10 19:18:49 +0200

    @RequestMapping Annotation

--- spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -1,25 +1,32 @@
 package net.javaguides.springannotations.controller;
 
 import net.javaguides.springannotations.beans.Book;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller
 //@ResponseBody
 @RestController
+@RequestMapping("/api")
 public class BookController {
 
     @RequestMapping("/hello-world")
     //@ResponseBody
-    public String helloWorld(){
+    public String helloWorld () {
         return "Hello World";
     }
 
-    @RequestMapping("/book")
+    @RequestMapping(value = {"/book", "compound-effect", "/self-help"},
+            method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
     //@ResponseBody
-    public Book getBook(){
+    public Book getBook () {
         Book book = new Book(1, "The Compound Effect", "The Compound Effect by Darren Hardy is a self-help book that shows how small, consistent actions—good or bad—compound over time to produce significant long-term results in your life, finances, health, and habits");
         return book;
     }
