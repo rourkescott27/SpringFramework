017179b26df023cedec65002bd6463ffa16c489c
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-10 19:25:20 +0200

    @GetMapping Annotation

--- spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -3,10 +3,7 @@ package net.javaguides.springannotations.controller;
 import net.javaguides.springannotations.beans.Book;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //@Controller
 //@ResponseBody
@@ -20,10 +17,8 @@ public class BookController {
         return "Hello World";
     }
 
-    @RequestMapping(value = {"/book", "compound-effect", "/self-help"},
-            method = RequestMethod.GET,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    @GetMapping(value = {"/book", "compound-effect", "/self-help"},
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE
     )
     //@ResponseBody
     public Book getBook () {
