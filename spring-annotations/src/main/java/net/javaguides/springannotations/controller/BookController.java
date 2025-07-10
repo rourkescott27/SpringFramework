c3c445eb54c7fca7877aa3567d7306093042074b
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-07-10 20:43:20 +0200

    @PostMapping & @RequestBody Annotations

--- spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -1,7 +1,9 @@
 package net.javaguides.springannotations.controller;
 
 import net.javaguides.springannotations.beans.Book;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +19,22 @@ public class BookController {
         return "Hello World";
     }
 
-    @GetMapping(value = {"/book", "compound-effect", "/self-help"},
-            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(value = {"/book", "compound-effect", "/self-help"})
     //@ResponseBody
     public Book getBook () {
         Book book = new Book(1, "The Compound Effect", "The Compound Effect by Darren Hardy is a self-help book that shows how small, consistent actions—good or bad—compound over time to produce significant long-term results in your life, finances, health, and habits");
         return book;
     }
-}
+
+    @PostMapping(value = "/books/create",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+//    @ResponseStatus(value =  HttpStatus.CREATED)
+    public ResponseEntity<Book> createBook ( @RequestBody Book book ) {
+        System.out.println(book.getId());
+        System.out.println(book.getTitle());
+        System.out.println(book.getDescription());
+
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+} 
